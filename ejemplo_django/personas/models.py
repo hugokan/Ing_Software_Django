9043113e4829c9eca98a5a81961dfeb90775bf5e
Hugo Quintero <hugojquinterop@gmail.com>
2019-09-15 16:30:05 -0300

    modelo persona

--- ejemplo_django/personas/models.py
+++ ejemplo_django/personas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    Nombre = models.CharField(max_length=100)
+    Apellido = models.CharField(max_length=100)
+    Telefono = models.IntegerField()
+    Email = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Persona"
+        verbose_name_plural = "Personas"
