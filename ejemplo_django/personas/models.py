8003207c8a883b2b864e09cd0fd7b5704f28e8ed
Hugo Quintero <hugojquinterop@gmail.com>
2019-09-16 11:13:15 -0300

    modificacion del models y se agrega el gitignore

--- ejemplo_django/personas/models.py
+++ ejemplo_django/personas/models.py
@@ -1,13 +1,33 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Persona(models.Model):
-    Nombre = models.CharField(max_length=100)
-    Apellido = models.CharField(max_length=100)
-    Telefono = models.IntegerField()
-    Email = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    telefono = models.IntegerField()
+    email = models.CharField(max_length=100)
 
-    class Meta:
-        verbose_name = "Persona"
-        verbose_name_plural = "Personas"
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    titulo = models.CharField('titulo', blank=False, max_length=150)
+    descripcion = models.TextField('Resumen', blank=True)
+    nombre = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    apellido = models.ForeignKey(Persona, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+class Compra(models.Model):
+    nombre = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    telefono = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    titulo = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    descripcion = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    fecha = models.DateTimeField(default=timezone.now)
+    hora = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.titulo
