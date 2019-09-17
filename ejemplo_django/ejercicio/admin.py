48742f8b94372f3bb4968ace59b189cd72106fde
Hugo Quintero <hugojquinterop@gmail.com>
2019-09-16 22:17:23 -0300

    mejoras en el admin y en models

--- ejemplo_django/ejercicio/admin.py
+++ ejemplo_django/ejercicio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Persona
+from .models import Producto
+from .models import Compra
 
-# Register your models here.
+admin.site.register(Persona)
+admin.site.register(Producto)
+admin.site.register(Compra)
--- ejemplo_django/ejercicio/models.py
+++ ejemplo_django/ejercicio/models.py
@@ -9,8 +9,8 @@ class Persona(models.Model):
     email = models.CharField(max_length=80, verbose_name="Email")
 
     class Meta:
-        verbose_name = "nombre"
-        verbose_name_plural = "nombres"
+        verbose_name = "cliente"
+        verbose_name_plural = "clientes"
 
     def __str__(self):
         return self.nombre
@@ -23,8 +23,8 @@ class Producto(models.Model):
     nombre = models.ForeignKey('persona', on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = "titulo"
-        verbose_name_plural = "titulos"
+        verbose_name = "producto"
+        verbose_name_plural = "productos"
 
     def __str__(self):
         return self.titulo
@@ -36,11 +36,11 @@ class Compra(models.Model):
     nombre = models.ForeignKey('persona', on_delete=models.CASCADE)
     fecha_compra = models.DateTimeField(
             default=timezone.now)
-    hora_compra = models.DateTimeField(
+    fecha_envio = models.DateTimeField(
             blank=True, null=True)
 
     def publish(self):
-        self.hora_compra = timezone.now()
+        self.fecha_envio = timezone.now()
         self.save()
 
     def __str__(self):
